872a0fd3735ae9bf50758d12d3a2350d87bc0d05
vishwajeet kumar <vishusrivastva@users.noreply.github.com>
2024-06-24 01:24:16 +0530

    updated auth middle ware response structure

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -18,12 +18,12 @@ const protect = async (req, res, next) => {
       next();
     } catch (error) {
       console.error(error);
-      res.status(401).json({ message: "Not authorized, token failed" });
+      res.status(401).json({ success: false, message: "Not authorized, token failed" });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: "Not authorized, no token" });
+    res.status(401).json({ success: false, message: "Not authorized, no token" });
   }
 };
 
